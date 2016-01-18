323bf21c33ffccfb987820f007eec19e5cbaf3f3
zfpx <zfpx@126.com>
2016-01-18 15:48:36 +0800

    request

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ##接口列表(scope)
 http://wiki.connect.qq.com/api%E5%88%97%E8%A1%A8
 
+
+#接口文档
+http://wiki.connect.qq.com/%E4%BD%BF%E7%94%A8authorization_code%E8%8E%B7%E5%8F%96access_token#Step1.EF.BC.9A.E8.8E.B7.E5.8F.96AuthorizationCode
--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 var express = require('express');
 var request = require('request');
+var querystring = require('querystring');
 var app = express();
 app.use(express.static(__dirname));
+var appId = '101283445';
 app.get('/re',function(req,res){
     console.log(req.url);
     var code = req.query.code;
     var url = 'https://graph.qq.com/oauth2.0/token';
     var qs = {
         grant_type:'authorization_code',
-        client_id:'101283445',
+        client_id:appId,
         client_secret:'abeba99edf2d36aa3b89043072d6e826',
         code:req.query.code,
         redirect_uri:'http://qq.zhufengpeixun.cn/re'
     };
-    request.get({url:url, oauth:{}, qs:qs, json:true}, function (e, r, user) {
-        console.log(user)
+    request.get({url:url, oauth:{}, qs:qs, json:true}, function (e, r, result) {
+        result = querystring.parse(result);
+        var access_token = result.access_token;
+        var accessUrl = 'https://graph.qq.com/oauth2.0/me';
+        request.get({url:accessUrl, oauth:{}, qs:{access_token:access_token}, json:true}, function (e, r, result) {
+            result = querystring.parse(result);
+            var clientId = result['client_id'];
+            var getInfoUrl = 'https://graph.qq.com/user/get_user_info';
+            var qs = {
+                access_token:  access_token,
+                oauth_consumer_key:appId,
+                openid:clientId
+            };
+            request.get({url:getInfoUrl, oauth:{}, qs:qs, json:true}, function (e, r, result) {
+                result = querystring.parse(result);
+                console.log(result);
+            })
+        })
     })
     res.end('hello');
 });
