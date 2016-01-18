46ad86a37ad9e8011334e4c53d82fbfff234bc34
zfpx <zfpx@126.com>
2016-01-18 14:57:14 +0800

    url

---
+++ README.md
@@ -0,0 +1,4 @@
+#URL
+##接口列表(scope)
+http://wiki.connect.qq.com/api%E5%88%97%E8%A1%A8
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
 app.get('/re',function(req,res){
+    console.log(req.url);
     res.end('hello');
 });
 app.listen(80);
