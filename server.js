af8f4226f7d12e8f8f234f0be840cf08e0eecfd3
zfpx <zfpx@126.com>
2016-01-18 15:25:44 +0800

    request

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 app.use(express.static(__dirname));
 app.get('/re',function(req,res){
     console.log(req.url);
+    var code = req.query.code;
+    var url = 'https://graph.qq.com/oauth2.0/me';
+    var qs = {access_token:req.query.code};
+    console.log(url);
+    request.get({url:url, oauth:{}, qs:qs, json:false}, function (e, r, result) {
+        console.log(arguments);
+        console.log(result)
+    })
     res.end('hello');
 });
 app.listen(80);
